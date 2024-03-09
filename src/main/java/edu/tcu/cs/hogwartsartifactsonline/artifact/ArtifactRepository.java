56dccef12c4cfeb331e0deac9ddf2b4552005a51
HIEN DAU <hien2003.dm@gmail.com>
2024-03-08 19:25:53 -0600

    Artifact CRUD done - Video 28! Close issue #1

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactRepository.java
@@ -0,0 +1,8 @@
+package edu.tcu.cs.hogwartsartifactsonline.artifact;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // this annotation is optional
+public interface ArtifactRepository extends JpaRepository<Artifact, String> {
+}
